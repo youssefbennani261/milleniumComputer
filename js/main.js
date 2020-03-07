e3604b1ff412c4d778b986c9165ada0e460797b9
simo <simo@gmail.com>
2020-03-07 15:38:31 +0100

    login completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 if(window.location.pathname==="/meliniumpc/ed9eee4c247fee6711a6e41d3dfe8bcd.php"){
     $("#login").click((e)=>{
         e.preventDefault();
-       $.post("php/login.php",{login:$("#email").val(),pwd:$("#pwd").val()},()=>{
+       $.post("php/login.php",{login:$("#email").val(),pwd:$("#pwd").val()},(data)=>{
+           if(data=="1"){
+               window.location.href="";
+           }
        })
     })
 }
--- php/login.php
+++ php/login.php
@@ -2,19 +2,18 @@
 require("connection.php");
 $pwd=isset($_POST["pwd"])?md5($_POST["pwd"]):"";
 $login=isset($_POST["login"])?$_POST["login"]:"";
-// $sql=mysqli_query($con,"select * from admin where login='".$login."'");
-$stmt = $con->prepare("select nom,prenom,email from admin where login=? and mot_Passe=?");
-     $stmt->bind_param("ss",$login,$pwd);
+$stmt = $con->prepare("select * from admin where (login=? or email=?) and mot_Passe=?");
+     $stmt->bind_param("sss",$login,$login,$pwd);
      $stmt->execute();
-     $stmt->bind_result($nom,$prenom,$email);
-     $stmt->store_result();
-     if($stmt->num_rows == 1) {
-          $result = $stmt->fetch();
-          
-}
-// print_r($sql->num_rows);
+     $result = $stmt->get_result();
+     if($result->num_rows){
+          session_start();
+          $_SESSION["admin"]=$result->fetch_assoc();
+          echo 1;
+     }
 
 
+$stmt->close();
 
 
 
