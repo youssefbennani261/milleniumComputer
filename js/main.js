4c23c43c1cd89f48a67bcf592b5035bad85ff80e
simo <simo@gmail.com>
2020-03-07 17:35:29 +0100

    ewq

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if(window.location.pathname==="/meliniumpc/ed9eee4c247fee6711a6e41d3dfe8bcd.php"
         e.preventDefault();
        $.post("php/login.php",{login:$("#email").val(),pwd:$("#pwd").val()},(data)=>{
            if(data=="1"){
-               window.location.href="";
+              alert("hello");
            }
        })
     })
