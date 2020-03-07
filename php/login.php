7f987dba3ab85f1dd6dd1ef2dd765a497962ed7f
simo <simo@gmail.com>
2020-03-07 20:18:13 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -2,9 +2,10 @@
 require("connection.php");
 $pwd=isset($_POST["pwd"])?md5($_POST["pwd"]):"";
 $login=isset($_POST["login"])?$_POST["login"]:"";
-// $sql=mysqli_query($con,"select * from admin where login='".$login."'");
-$stmt = $con->prepare("select * from admin where (login=? or email=?) and mot_Passe=?");
-     $stmt->bind_param("sss",$login,$login,$pwd);
+
+
+$stmt = $con->prepare("select * from admin where login=? and mot_Passe=?");
+     $stmt->bind_param("ss",$login,$pwd);
      $stmt->execute();
      $result = $stmt->get_result();
      if($result->num_rows){
